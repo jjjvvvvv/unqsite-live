817652079c2b0efd65ce1b3f37aec0918e9e7200
Jon Virga <57054770+jjjvvvvv@users.noreply.github.com>
2020-02-11 23:06:24 -0500

    Update settings.py

--- unqsite/settings.py
+++ unqsite/settings.py
@@ -131,12 +131,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-#STATIC_ROOT = '/assets/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/staticfiles/'
 
 MEDIA_URL = '/img/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
